433301db226e10c0f93b8935046ef2e249149471
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-12-01 20:06:13 +0100

    user login

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -3,19 +3,45 @@
 import React, { useState } from "react";
 import Button from "@/app/components/Button";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const router = useRouter();
+  const [mesasge, setMessage] = useState("");
+  // const router = useRouter();
 
-  const handleLogin = (e: React.FormEvent) => {
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     // Handle login logic here
-    console.log("Username:", username);
-    console.log("Password:", password);
-    router.push("/dashboards/admin");
+    // console.log("Username:", username);
+    // console.log("Password:", password);
+    // router.push("/dashboards/admin");
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+      if (response.ok) {
+        setMessage("Login succesful!");
+        // handle succesfull login
+      } else {
+        const errorMessage = await response.text();
+        setMessage(`Login Failed: ${errorMessage}`);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setMessage(`An error has occured: ${error.message}`);
+      } else {
+        setMessage(`An unexpected error occured`);
+      }
+    }
   };
 
   return (
@@ -49,6 +75,7 @@ const Login = () => {
           Login
         </Button>
       </form>
+      {mesasge && <p>{mesasge}</p>}
       <div className="text-white py-2">
         Nog geen account? Registreer je <Link href={"/register"}>hier.</Link>
       </div>
